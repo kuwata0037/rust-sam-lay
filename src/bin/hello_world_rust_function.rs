2ebead94a03b1e33458013d6209940da48dcefeb
kuwata0037 <kuwata0037@gmail.com>
2021-07-30 21:46:18 +0900

    クエリパラメータのキーの指定方法を修正

--- src/bin/hello_world_rust_function.rs
+++ src/bin/hello_world_rust_function.rs
@@ -11,13 +11,11 @@ async fn main() -> Result<(), Error> {
 }
 
 async fn func(event: Request, _: Context) -> Result<impl IntoResponse, Error> {
-    Ok(
-        match event.query_string_parameters().get("{{project-name}}") {
-            Some(project_name) => format!("Hello, {}!", project_name).into_response(),
-            _ => Response::builder()
-                .status(400)
-                .body("Empty project name".into())
-                .expect("failed to render response"),
-        },
-    )
+    Ok(match event.query_string_parameters().get("project_name") {
+        Some(project_name) => format!("Hello, {}!", project_name).into_response(),
+        _ => Response::builder()
+            .status(400)
+            .body("Empty project name".into())
+            .expect("failed to render response"),
+    })
 }
